2cf34c403b3b68c8fcf0fff3111fdf0068fe935b
MaGadev69 <maxigasquez@gmail.com>
2025-05-09 01:14:28 -0300

    Agrego estáticos del frontend

--- myapp/myapp.py
+++ myapp/myapp.py
@@ -1,29 +1,18 @@
-# Welcome to Reflex! This file outlines the steps to create a basic app
-
 import reflex as rx
+from myapp.states.stock_state import StockState
 
-from .pages import index
-from .pages import health
-from .pages import not_found
 
-from .api import root
+def index() -> rx.Component:
+    return rx.el.div(
+        rx.el.p(StockState.cantidad_activos),
+        rx.cond(
+            StockState.error_message != "",
+            rx.el.p(StockState.error_message),
+            None,
+        ),
+        on_mount=StockState.fetch_cantidad_activos,
+    )
 
-app = rx.App()
 
+app = rx.App(theme=rx.theme(appearance="light"))
 app.add_page(index)
-app.add_page(health)
-
-#app.api.add_api_route(
-#    path="/",
-#    endpoint=root
-#)
-
-not_found_text = "The page you were looking for could not be found"
-
-app.add_custom_404_page(
-    title="404 - Page Not Found", 
-    description=not_found_text,
-    component=not_found(not_found_text)
-)
-if __name__ == "__main__":
-    app.compile()
